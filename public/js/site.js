afd29ea4c5afc16df709c49f6a9a7a8a2e69bcd3
Matt Bullen <burner0110@gmail.com>
2015-04-25 17:02:59 -0700

    .

--- public/js/site.js
+++ public/js/site.js
@@ -98,7 +98,7 @@ $(document).ready(function() {
     /*
         Submit the form without reloading the page. Source:
             http://stackoverflow.com/questions/22163220/prevent-page-reload-after-form-submit-node-no-ajax-available
-    
+    */
     $("#text-message-form").submit(function(e) {
         e.preventDefault();
         var fd = new FormData($(this)[0]);
@@ -113,7 +113,7 @@ $(document).ready(function() {
             }
         });
     });
-    */
+    
 }); // End $(document).ready();
 
 
